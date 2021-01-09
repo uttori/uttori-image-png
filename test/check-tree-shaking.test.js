8fede4982a760dd21cf6b987ca23af204054339a
Matthew Callis <matthew.callis@gmail.com>
2021-01-08 19:50:52 -0800

    WIP Allowing tree-shaking of deps for browser builds

---
+++ test/check-tree-shaking.test.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const test = require('ava');
+
+// https://rollupjs.org/guide/en/#javascript-api
+const rollup = require('rollup');
+const commonjs = require('@rollup/plugin-commonjs');
+const replace = require('@rollup/plugin-replace');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+
+const onwarn = () => {};
+const plugins = [
+  nodeResolve({
+    mainFields: ['module', 'main'],
+  }),
+  replace({
+    'process.env.UTTORI_DATA_DEBUG': 'false',
+    'process.env.UTTORI_IMAGEPNG_DEBUG': 'false',
+  }),
+  commonjs(),
+];
+
+// Debugging File Output
+// await bundle.write({
+//   file: './test/tree-shaking-output.js',
+//   format: 'es',
+// });
+
+test('Tree Shaking: { DataBuffer, DataBufferList, DataStream }', async (t) => {
+  const bundle = await rollup.rollup({
+    input: './test/tree-shaking/shake-me.mjs',
+    onwarn,
+    plugins,
+  });
+
+  const output = await bundle.generate({
+    format: 'es',
+  });
+
+  // Sum should be (1 (input) + 3 (data tools) + 16 pako files) number of expected modules
+  t.deepEqual(Object.keys(output.output[0].modules).map((f) => path.basename(f)), [
+    'adler32.js',
+    'crc32.js',
+    'inffast.js',
+    'inftrees.js',
+    'constants.js',
+    'inflate.js',
+    'common.js',
+    'strings.js',
+    'messages.js',
+    'zstream.js',
+    'gzheader.js',
+    'inflate.js',
+    'data-buffer.js',
+    'data-buffer-list.js',
+    'data-stream.js',
+    'data-image-png.js',
+    'shake-me.mjs',
+  ]);
+});
---
+++ test/tree-shaking/shake-me.mjs
@@ -0,0 +1,12 @@
+/* eslint-disable node/no-unpublished-import */
+/* eslint-disable import/extensions */
+/* eslint-disable unicorn/import-index */
+import ImagePNG from '../../esm/index.js';
+
+const main = () => {
+  const png = new ImagePNG();
+
+  return [png];
+};
+
+export default main;
